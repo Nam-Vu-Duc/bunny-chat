f8f7e3955d6a24ea86798d4e7169a6932da756ae
namvu <vuducnama6@example.com>
2025-02-12 21:27:03 +0700

    update url #2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000/, https://bunny-store.vercel.app/"],
+    origin: ["http://localhost:3000", "https://bunny-store.vercel.app"],
     methods: ["GET", "POST"]
   }
 });
